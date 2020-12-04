1792f22555b8925c9a7aadbabd9c8fffd7290488
nguyenthaitan <nguyenthaitan9@gmail.com>
2020-12-05 00:07:53 +0700

    process env

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 8002
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8002;
 
 app.get('/', (req, res) => {
   res.send('Hello World! Welcome to Team 3.')
