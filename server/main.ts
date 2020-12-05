4217f165aaf1ef68ea9aa564715802b1a5390720
nguyenthaitan <nguyenthaitan9@gmail.com>
2020-12-05 14:06:26 +0700

    use api

--- server/main.ts
+++ server/main.ts
@@ -1,6 +1,9 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as userApi from "./modules/users/user.api";
+import * as promotionApi from "./modules/promotions/promotion.api";
+import * as orderApi from "./modules/orders/order.api";
+import * as foodListApi from "./modules/foodLists/foodList.api";
 
 async function initServer() {
   const app = express();
@@ -8,7 +11,11 @@ async function initServer() {
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
   app.use("/user", userApi);
+  app.use("/promotion", promotionApi);
+  app.use("/order", orderApi);
+  app.use("/food-list", foodListApi);
 
   app.get("/", (req, res) => {
     res.send("Hello World! Welcome to Team 3!!!");
